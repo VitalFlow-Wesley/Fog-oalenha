db55200a65ff0524d2cb79ee6f1898bf29a8db7d
Wesley Peixoto Alves <wesley310189@gmail.com>
2026-06-04 11:01:54 -0300

    feat: add initial users data

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export const initialUsers = [
+  {
+    id: 1,
+    name: 'Administrador',
+    username: 'admin',
+    password: 'admin123',
+    role: 'admin',
+    active: true
+  },
+  {
+    id: 2,
+    name: 'Gerente',
+    username: 'gerente',
+    password: 'gerente123',
+    role: 'gerente',
+    active: true
+  },
+  {
+    id: 3,
+    name: 'Garçom',
+    username: 'garcom',
+    password: 'garcom123',
+    role: 'garcom',
+    active: true
+  }
+]
