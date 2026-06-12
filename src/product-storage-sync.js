6301e716e05012a31dedecf0e665bb02267f5a3b
Wesley Peixoto Alves <wesley310189@gmail.com>
2026-06-12 12:10:42 -0300

    Carrega ajustes visuais do historico de mesas fechadas

--- src/product-storage-sync.js
+++ src/product-storage-sync.js
@@ -1,5 +1,6 @@
 import './encoding-repair-runtime.js'
 import './native-edit-modal-runtime.js'
+import './closed-tables-modal-polish.css'
 import { repairData, repairText } from './text-normalizer.js'
 
 const PRODUCT_CONFIG_KEY = 'fogao-a-lenha-products-settings'
